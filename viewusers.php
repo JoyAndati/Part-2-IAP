3ff22bdff8b4f4c371b341a6499526acfb32f247
LeonMunene <leon.bundi@strathmore.edu>
2023-09-23 21:56:16 +0300

    update viewusers.php

--- viewusers.php
+++ viewusers.php
@@ -10,6 +10,34 @@
         // Bind the result to a variable
         $stmt->bind_result($fullname);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/webstructure.css"/>
+    <title>Document</title>
+</head>
+<body>
+<div class="topnav">
+    <a href="index.html">Home</a>
+    <a href="aboutUs.html">About us</a>
+    <a href="viewBlog.html">Blog</a>
+    <a href="contactus.html">Contact us</a>
+    <a href="ourProducts.html">Our Products</a>
+    <div class="topnav_right">
+    <a href="viewusers.php">View Users</a>  
+    <a href="signIn.php">Sign in</a>
+    <a href="signUp.php">Sign up</a>
+    <i class="fa-brands fa-meetup fa-beat fa-2xl" style="color: #51371f;"></i>
+
+    </div> 
+</div>
+
+<div class="header">
+    <h1>Users List</h1>
+</div>
+
 
 
 
